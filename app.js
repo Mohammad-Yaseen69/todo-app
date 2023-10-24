ea8f1cd2482da21e2a75bae3e9c3adf025463934
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2023-10-24 20:12:16 +0500

    adding local storage functionality in to the app

--- app.js
+++ app.js
@@ -1,42 +1,81 @@
 var taskInput = document.getElementById("task-input");
 var listParent = document.getElementById("List");
+var tasks = [];
+
+loadTasksFromLocalStorage();
 
 function addTask() {
-    var li = document.createElement("li");
-    var spanTag = document.createElement("span");
-    spanTag.innerHTML = taskInput.value
+    var taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        var task = {
+            text: taskText
+        };
+        tasks.push(task);
+        saveTasksToLocalStorage();
+        displayTasks();
+        taskInput.value = "";
+    }
+}
 
-    //  creating Edit button
-    var editBtn = document.createElement("button");
-    editBtn.innerHTML = "Edit";
-    editBtn.setAttribute("onclick","editButton(this)");  
-    
-    // creating Delete button 
-    var deleteBtn = document.createElement("button")
-    deleteBtn.innerHTML = "Delete";
-    deleteBtn.className = "Delete";
-    deleteBtn.setAttribute("onclick","deleteButton(this)"); 
-    
-    if (taskInput.value !== "" && taskInput.value !== " ") {
-        listParent.appendChild(li);
+function displayTasks() {
+    listParent.innerHTML = "";
+    for (var i = 0; i < tasks.length; i++) {
+        var li = document.createElement("li");
+        var spanTag = document.createElement("span");
+        spanTag.innerHTML = tasks[i].text;
+
+        var editBtn = document.createElement("button");
+        editBtn.innerHTML = "Edit";
+        editBtn.setAttribute("onclick", "editButton(this)");
+
+        var deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Delete";
+        deleteBtn.className = "Delete";
+        deleteBtn.setAttribute("onclick", "deleteButton(this)");
+
         li.appendChild(spanTag);
         li.appendChild(editBtn);
-        li.appendChild(deleteBtn)
+        li.appendChild(deleteBtn);
+        listParent.appendChild(li);
     }
-
-    taskInput.value = ""
 }
 
 function delAll() {
-    listParent.innerHTML = " "
+    tasks = [];
+    saveTasksToLocalStorage();
+    listParent.innerHTML = "";
 }
-function editButton(e) {
 
-    // console.log()
+function editButton(e) {
     var editPrompt = prompt("Enter the New Task");
-    e.parentNode.firstChild.innerHTML = editPrompt
-    
+    if (editPrompt !== null) {
+        var index = tasks.findIndex(task => task.text === e.parentNode.firstChild.innerHTML);
+        if (index !== -1) {
+            tasks[index].text = editPrompt;
+            saveTasksToLocalStorage();
+            displayTasks();
+        }
+    }
 }
-function deleteButton(e){
-    e.parentNode.remove();
+
+function deleteButton(e) {
+    var taskText = e.parentNode.firstChild.innerHTML;
+    var index = tasks.findIndex(task => task.text === taskText);
+    if (index !== -1) {
+        tasks.splice(index, 1);
+        saveTasksToLocalStorage();
+        e.parentNode.remove();
+    }
+}
+
+function saveTasksToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasksFromLocalStorage() {
+    var storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+        tasks = JSON.parse(storedTasks);
+        displayTasks();
+    }
 }
